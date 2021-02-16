27fb24568ec8bc2ad97f245dfaebf4db5094d59d
Adam Brent <adam.brent@gmail.com>
2021-02-15 22:41:49 -0800

    Adds initial schema.

--- database/index.js
+++ database/index.js
@@ -23,21 +23,130 @@ const characterSchema = new mongoose.Schema({
     wisdom: {value: Number, mod: Number},
     charisma: {value: Number, mod: Number},
   },
+  inspiration: String,
+  proficiencyBonus: Number,
+  savingThrows: {
+    strength: {value: Number, proficient: Boolean},
+    dexterity: {value: Number, proficient: Boolean},
+    constitution: {value: Number, proficient: Boolean},
+    intelligence: {value: Number, proficient: Boolean},
+    wisdom: {value: Number, proficient: Boolean},
+    charisma: {value: Number, proficient: Boolean},
+  },
+  skills: {
+    acrobatics: {value: Number, proficient: Boolean},
+    animalHandling: {value: Number, proficient: Boolean},
+    arcana: {value: Number, proficient: Boolean},
+    athletics: {value: Number, proficient: Boolean},
+    deception: {value: Number, proficient: Boolean},
+    history: {value: Number, proficient: Boolean},
+    insight: {value: Number, proficient: Boolean},
+    intimidation: {value: Number, proficient: Boolean},
+    investigation: {value: Number, proficient: Boolean},
+    medicine: {value: Number, proficient: Boolean},
+    nature: {value: Number, proficient: Boolean},
+    perception: {value: Number, proficient: Boolean},
+    performance: {value: Number, proficient: Boolean},
+    persuasion: {value: Number, proficient: Boolean},
+    religion: {value: Number, proficient: Boolean},
+    slightOfHand: {value: Number, proficient: Boolean},
+    stealth: {value: Number, proficient: Boolean},
+    survival: {value: Number, proficient: Boolean},
+  },
+  otherProficiencesAndLanguages: String,
+  passivePerception: Number,
+  passiveInvestigation: Number,
+  passiveInsight: Number,
+  armorClass: Number,
+  initiative: Number,
+  speed: Number,
+  hp: {
+    current: Number,
+    max: Number,
+    temporary: Number,
+  },
+  hitDice: {
+    current: String,
+    total: String,
+  },
+  deathSaves: {
+    successes: Number,
+    failures: Number,
+  },
+  attacks: [
+    {
+      name: String,
+      attackBonus: Number,
+      damage: String,
+    }
+  ],
+  attackNotes: String,
+  gear: {
+    money: {
+      cp: Number,
+      sp: Number,
+      ep: Number,
+      gp: Number,
+      pp: Number,
+    },
+    equipment: String,
+  },
+  personalityTraits: String,
+  ideals: String,
+  bonds: String,
+  flaws: String,
+  featuresAndTraits: String,
+  additionalFeaturesAndTraits: String,
+  age: Number,
+  height: String,
+  weight: String,
+  eyes: String,
+  skin: String,
+  hair: String,
+  appearance: {
+    description: String,
+    image: String,
+  },
+  alliesAndOrganizations: String,
+  symbol: {
+    name: String,
+    image: String,
+  }
+  treasure: String,
+  spellcasting: {
+    class: String,
+    ability: String,
+    saveDC: Number,
+    attackBonus: Number,
+  },
+  cantrips: [
+    {
+      name: String,
+      school: String,
+    }
+  ],
+  spells: [
+    {
+      level: Number,
+      slots: {
+        total: Number,
+        expended: Number,
+      },
+      spellsKnown: [
+        {
+          name: String,
+          school: String,
+          components: String,
+          prepared: Boolean,
+        },
+      ]
+    }
+  ]
   location: {
     city: String,
     state: String,
     country: String,
   },
-  gallery: [
-    {
-      _id: Number,
-      photoName: String,
-      photoUrl: String,
-      photoDescription: String,
-      isVerified: Boolean,
-      hasDescription: Boolean,
-    },
-  ],
 });
 
 const Character = mongoose.model('Character', characterSchema);
